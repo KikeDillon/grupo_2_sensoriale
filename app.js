b083dffc2942b81dcf0089edc246141171fc82ca
Cristian López Smail <cristian.ls@outlook.com>
2021-01-28 00:45:27 -0300

    index.html y su css, se arman los contenedores

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require ('express');
+const app = express ();
+
+const path = require ('path');
+const publicpath = path.resolve (__dirname, './public');
+
+app.use (express.static(publicpath));
+
+app.set ('puerto', process.env.PORT || 3000);
+
+app.listen (app.get ('puerto'), function(){
+    console.log ("LEVANTÓ EL SERVIDOR");
+});
+
+app.get('/', function(req, res){
+    res.sendFile (path.resolve(__dirname, './views/index.html'));
+});
+
+app.get('/register', function(req, res){
+    res.sendFile (path.resolve(__dirname, './views/register.html'));
+});
+
+app.get('/login', function(req, res){
+    res.sendFile (path.resolve(__dirname, './views/login.html'));
+});
+
+app.get('/checkout', function(req,res){
+    res.sendFile (path.resolve(__dirname, './views/checkout.html'));
+});
